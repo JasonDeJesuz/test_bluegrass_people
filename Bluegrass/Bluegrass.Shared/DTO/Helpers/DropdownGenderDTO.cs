ef9ea54d81dca4fcff6f5b95b6df0b30f825bcbe
Jason De Jesuz <dejesuzjason@gmail.com>
2022-05-20 01:20:45 +0200

    Add ? to Gender String

--- Bluegrass/Bluegrass.Shared/DTO/Helpers/DropdownGenderDTO.cs
+++ Bluegrass/Bluegrass.Shared/DTO/Helpers/DropdownGenderDTO.cs
@@ -5,7 +5,7 @@ namespace Bluegrass.Shared.DTO.Helper
     public class DropdownGenderDTO
     {
         public int Id { get; set; }
-        public string Gender { get; set; }
+        public string? Gender { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
     }
